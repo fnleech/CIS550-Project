28c26977af8fdb1de0743d02292c2ee83abf3ed3
Lanqing Yang <lanqingy@seas.upenn.edu>
2017-03-28 17:40:40 -0400

    python script

--- Casesdump.py
+++ Casesdump.py
@@ -1,12 +1,11 @@
 import cx_Oracle
 import csv
 rows = []
-with open('FinalData/Cases.csv', 'rb') as csvfile:
+with open('C:\Studying Folder\CIS 550\pj\CIS550-Project\FinalData\Cases.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for line in spamreader:
-        row = (line[0], line[1])
+        row = (line[0], line[1], line[2], line[3])
         rows.append(row)
-
 
 # insert all of the rows as a batch and commit
 host = 'cis550project.cgajnbzkqq1i.us-west-2.rds.amazonaws.com' 
@@ -16,7 +15,7 @@ database_table_name = 'Cases'
 dsn = cx_Oracle.makedsn(host, port, SID)
 connection = cx_Oracle.connect('cis550project', 'cis550projectkeyPENN', dsn)
 cursor = cx_Oracle.Cursor(connection)
-cursor.prepare('insert into ' + database_table_name+ ' (StateCode,CaseNum, CaseDate, CaseType) values (:1, :2, :3, :4)')
+cursor.prepare('insert into ' + database_table_name + ' (StateCode, CaseNum, CaseDate, CaseType) values (:1, :2, :3, :4)')
 cursor.executemany(None, rows)
 connection.commit()
 cursor.close()
--- PredictedDump.py
+++ PredictedDump.py
@@ -1,22 +1,25 @@
+#oracle connection separate package we need to install
 import cx_Oracle
+
+#with python, use CSV reader 
 import csv
 rows = []
-with open('FinalData/Predicted.csv', 'rb') as csvfile:
+with open('C:\Studying Folder\CIS 550\pj\CIS550-Project\FinalData\Predicted.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for line in spamreader:
         row = (line[0], line[1])
         rows.append(row)
 
-
 # insert all of the rows as a batch and commit
+
 host = 'cis550project.cgajnbzkqq1i.us-west-2.rds.amazonaws.com' 
 port = 1521
 SID = 'PENNDB'
-database_table_name = 'State'
+database_table_name = 'CASEPREDICT'
 dsn = cx_Oracle.makedsn(host, port, SID)
 connection = cx_Oracle.connect('cis550project', 'cis550projectkeyPENN', dsn)
 cursor = cx_Oracle.Cursor(connection)
-cursor.prepare('insert into ' + STATE+ ' (StateCode, Predict) values (:1, :2)')
+cursor.prepare('insert into ' + database_table_name + ' (StateCode, Predict) values (:1, :2)')
 cursor.executemany(None, rows)
 connection.commit()
 cursor.close()
