c70e5825b0a4fa0cb633a135f58fc317581882ac
wglisson <wfglisson@gmail.com>
2017-03-28 16:30:00 -0400

    Updated DDL with CaseType, constraints

--- db_tables.sql
+++ db_tables.sql
@@ -4,20 +4,21 @@ StateCode VARCHAR(225),
 PRIMARY KEY (StateCode)
 );
 CREATE TABLE Cases (
-StateCode VARCHAR(2),
+StateCode VARCHAR(225),
 CaseNum int, 
-CaseDate date,  
+CaseDate date,
+CaseType VARCHAR(12),
 PRIMARY KEY (StateCode, CaseDate),
 FOREIGN KEY (StateCode) REFERENCES State(StateCode)
 );
 CREATE TABLE CaseCount (
-StateCode VARCHAR(2),
+StateCode VARCHAR(225),
 CaseNum int, 
 PRIMARY KEY (StateCode, CaseNum),
 FOREIGN KEY (StateCode) REFERENCES State(StateCode)
 );
 CREATE TABLE CasePredict(
-StateCode VARCHAR(2),
+StateCode VARCHAR(225),
 Predict int,
 PRIMARY KEY (StateCode, Predict),
 FOREIGN KEY (StateCode) REFERENCES State(StateCode)
