12eba27614c359bdee6bdcc346264da309f51721
Francine Leech <francinenleech@gmail.com>
2017-04-21 13:15:15 -0400

    OlympicData

--- OlympicData/Event_script.R
+++ OlympicData/Event_script.R
@@ -1,4 +1,18 @@
 setwd("/Users/francineleech/Github/CIS550-Project/OlympicData")
-datraw = read.csv("Eventraw.csv", header = TRUE)
-datfinal =datraw[duplicated(datraw),]
-write.csv(datfinal, file = "EventFinal.csv")
+datrio = read.csv("events.csv", header = TRUE)
+datbeg = read.csv("2008EVENTS.csv", header = F)
+
+datbeg_final =unique(datbeg)
+
+#Find what is in Bejing not in Rio
+install.packages("data.table")
+library(data.table)
+
+DT1 <- data.table(datrio)
+DT2 <- data.table(cbind(datbeg_final, 0), key=paste0("V", seq(len=ncol(datbeg_final))))
+setnames(DT1, c(head(names(DT1), -1L), "found"))
+DT2[DT1, list(found=ifelse(is.na(found), 0, 1))]
+
+# Beijing found in Rio 
+
+write.csv(datrio, file = "Sport.csv")
