72e4a8264665df7b766bbfae486bbf3090e91866
Lanqing Yang <lanqingy@seas.upenn.edu>
2017-04-24 12:31:12 -0400

    update DDLs

--- OlympicData/FinalData/Participates.py
+++ OlympicData/FinalData/Participates.py
@@ -7,7 +7,7 @@ rows = []
 with open('C:\Studying Folder\CIS 550\pj\CIS550-Project\OlympicData\FinalData\participates.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for line in spamreader:
-        row = (line[0], line[1], line[2])
+        row = (line[1], line[0], line[2])
         rows.append(row)
 
 # insert all of the rows as a batch and commit
@@ -19,7 +19,7 @@ database_table_name = 'Participates'
 dsn = cx_Oracle.makedsn(host, port, SID)
 connection = cx_Oracle.connect('cis550project', 'cis550projectkeyPENN', dsn)
 cursor = cx_Oracle.Cursor(connection)
-cursor.prepare('insert into ' + database_table_name + ' (Year, CID, Role) values (:2, :1, :3)')
+cursor.prepare('insert into ' + database_table_name + ' (Year, CID, Role) values (:1, :2, :3)')
 cursor.executemany(None, rows)
 connection.commit()
 cursor.close()
