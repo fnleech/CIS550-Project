f802729d1a7816b2373f838c620eefc15b7fe3a4
Lanqing Yang <lanqingy@seas.upenn.edu>
2017-04-24 10:39:25 -0400

    Country data version 2

--- OlympicData/Country.py
+++ OlympicData/Country.py
@@ -7,7 +7,7 @@ rows = []
 with open('C:\Studying Folder\CIS 550\pj\CIS550-Project\OlympicData\FinalData\Country_v2.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for line in spamreader:
-        row = (line[0], line[1], line[2], line[3], line[4], line[5], line[6])
+        row = (line[0], line[1], line[4], line[2], line[3], line[5], line[6])
         rows.append(row)
 
 # insert all of the rows as a batch and commit
@@ -19,7 +19,7 @@ database_table_name = 'Country'
 dsn = cx_Oracle.makedsn(host, port, SID)
 connection = cx_Oracle.connect('cis550project', 'cis550projectkeyPENN', dsn)
 cursor = cx_Oracle.Cursor(connection)
-cursor.prepare('insert into ' + database_table_name + ' (CID, Name, Continent, Population, GDP, PM10, PM25) values (:1, :2, :5, :3, :4, :6, :7)')
+cursor.prepare('insert into ' + database_table_name + ' (CID, Name, Continent, Population, GDP, PM10, PM25) values (:1, :2, :3, :4, :5, :6, :7)')
 cursor.executemany(None, rows)
 connection.commit()
 cursor.close()
