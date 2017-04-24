f1551e386581f0a62886cd0ae727c0e567894d2e
Lanqing Yang <lanqingy@seas.upenn.edu>
2017-04-24 11:31:05 -0400

    updated Sport Compete_in data and DDLs

--- OlympicData/FinalData/Sport.py
+++ OlympicData/FinalData/Sport.py
@@ -4,10 +4,10 @@ import cx_Oracle
 #with python, use CSV reader 
 import csv
 rows = []
-with open('C:\Studying Folder\CIS 550\pj\CIS550-Project\OlympicData\FinalData\Sport.csv', 'rb') as csvfile:
+with open('C:\Studying Folder\CIS 550\pj\CIS550-Project\OlympicData\FinalData\Sport_v2.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for line in spamreader:
-        row = (line[0], line[1], line[2], line[3])
+        row = (line[0], line[1])
         rows.append(row)
 
 # insert all of the rows as a batch and commit
@@ -19,7 +19,7 @@ database_table_name = 'Sport'
 dsn = cx_Oracle.makedsn(host, port, SID)
 connection = cx_Oracle.connect('cis550project', 'cis550projectkeyPENN', dsn)
 cursor = cx_Oracle.Cursor(connection)
-cursor.prepare('insert into ' + database_table_name + ' (Name, Gender, Discipline, Event) values (:1, :4, :2, :3)')
+cursor.prepare('insert into ' + database_table_name + ' (Discipline, Event) values (:1, :2)')
 cursor.executemany(None, rows)
 connection.commit()
 cursor.close()
