5f81ca9051b0d1088e196574775d524b804620c8
Xilei Kuang <apple@vagvlan536.0683.wlan.vpul.upenn.edu>
2017-04-27 17:26:49 -0400

    title

--- MyAppNode/routes/index.js
+++ MyAppNode/routes/index.js
@@ -5,7 +5,7 @@
 
 exports.do_work = function(req, res){
   res.render('index.jade', { 
-	  title: 'Please enter a person login' 
+	  title: 'Olympics' 
   });
 };
 exports.do_ref = function(req, res){
