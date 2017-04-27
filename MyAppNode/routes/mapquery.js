a5b0a4c0542964b0ec5646567e608a911ee6cfa7
Francine Leech <francinenleech@gmail.com>
2017-04-27 11:08:59 -0400

    Mapsquery
    Store map results

---
+++ MyAppNode/routes/mapquery.js
@@ -0,0 +1,35 @@
+var mapresults = {connection.query("Select C.CID, COALESCE(Count(R.Medal), 0)
+From Athlete A, Country C, AFromC AFC, Result R
+Where C.CID = AFC.CID 
+      and A.AID = AFC.AID 
+      and A.AID = R.AID 
+      and R.Medal = 'Gold'
+Group by C.CID
+Order by C.CID", function(err, rows){
+  if(err) {
+    throw err;
+  } else {
+    setValue(rows);
+  }
+	});}
+
+//query for the results
+
+var mapresults = [
+	2,
+	5,
+	2,
+	2,
+	2
+];
+
+// pass the results from  here to map.jade
+function display_mapresults(res, mapresults) {
+	res.render('map.jade',
+		   { mapresults: JSON.stringify(mapresults) }
+	  );
+}
+
+// if 
+//a.properties.name == local_mapresuts[i,1]
+// then print local_mapresults[i,2]
