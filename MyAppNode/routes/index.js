0ce3aa195648b94e1485c5edd868a977993330ac
Lanqing Yang <lanqingy@seas.upenn.edu>
2017-04-27 23:27:09 -0400

    merging

--- MyAppNode/routes/index.js
+++ MyAppNode/routes/index.js
@@ -21,13 +21,11 @@ exports.do_olym = function(req, res){
   res.render('olympics.jade', {});
 };
 
-<<<<<<< HEAD
 exports.do_map = function(req, res){
   res.render('map.jade', {});
-=======
+
 exports.do_coun = function(req, res){
   res.render('country.jade', {});
->>>>>>> cd7c797c53fcd4f363dd271f24eff1d8bbb92dcd
 };
 //exports.do_quiz = function(req, res){
   //res.render('quiz.jade', {});
