7fc6a3d04352e8656938960f2b1efe0be97ffe62
wglisson <wfglisson@gmail.com>
2017-04-30 21:21:55 -0400

    test

--- MyAppNode/routes/country.js
+++ MyAppNode/routes/country.js
@@ -1,7 +1,8 @@
 var async = require('async');
 var oracledb = require('oracledb');
 var global_res;
-var global_login;
+var global_search;
+var global_info;
 
 var doconnect = function (cb) {
 	oracledb.outFormat = oracledb.OBJECT;
@@ -22,32 +23,60 @@ var dorelease = function(conn) {
 };
 
 
-var query_country = function (conn, cb) {
+var query_athlete = function (conn, cb) {
+    console.log(global_search);
   conn.execute(
-    "SELECT * FROM COUNTRY",
+    "SELECT * FROM ATHLETE WHERE FULLNAME=" + global_search,
     function(err, result)
     {
       if (err) {
         return cb(err, conn);
       } else {
-				console.log(result.rows);
-        output_table(global_res, result.rows);
-        return cb(null, conn);
+            global_info = result.rows;
+            if (global_info.length > 0) {
+                global_search = global_info[0].AID;
+                return cb(null, conn);
+            } else {
+                global_search = null;
+                athlete_error(global_res);
+                return cb(null, conn);
+            }
+            
       }
     });
 };
 
+var query_results = function (conn, cb) {
+  if (global_search === null) {
+      return cb(null, conn);
+  } else {
+  conn.execute(
+    "SELECT * FROM RESULT WHERE AID=:name",
+    [global_search],
+    function(err, result)
+    {
+      if (err) {
+        return cb(err, conn);
+      } else {
+            console.log(global_info);
+            console.log(result.rows);
+            output_table(global_res, global_info, result.rows);
+            return cb(null, conn);
+      }
+    })};
+};
+
 /////
-// Query the oracle database, and call output_actors on the results
+// Query the oracle database
 //
 // res = HTTP result object sent back to the client
-// name = Name to query for
 function query_db(res) {
 	global_res = res;
 	async.waterfall(
 		[
 			doconnect,
-			query_country
+			query_athlete,
+            query_results
 		],
 		function (err, conn) {
 			if (err) { console.error("In waterfall error cb: ==>", err, "<=="); }
@@ -61,16 +90,25 @@ function query_db(res) {
 // Given a set of query results, output a table
 //
 // res = HTTP result object sent back to the client
-// name = Name to query for
+// info = object with athlete info results
 // results = List object of query results
-function output_table(res,results) {
-	res.render('query.jade',
-		   { results: results }
+function output_table(res,info,results) {
+    console.log(results);
+	res.render('athquery.jade',
+		   {   info: info,
+               results: results }
 	  );
+}
+
+function athlete_error(res) {
+    res.render('athlete.jade',
+        { error: 'an error' }
+    )
 }
 
 /////
 // This is what's called by the main app 
 exports.do_work = function(req, res){
-		query_db(res);
+    global_search = "'" + req.body.name + "'";
+    query_db(res);
 };
